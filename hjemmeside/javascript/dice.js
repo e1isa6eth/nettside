21cdf587b32a859f0a50f9a0cf826aa02637e5ec
Elisabeth K <111966217+e1isa6eth@users.noreply.github.com>
2023-04-24 12:57:42 +0200

    Add files via upload

---
+++ hjemmeside/javascript/dice.js
@@ -0,0 +1,17 @@
+function randomImages(){ 
+
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    document.querySelector(".img1").setAttribute("src", "/hjemmeside/img/dice" + randomNumber1 + ".png")
+ 
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    document.querySelector(".img2").setAttribute("src", "/hjemmeside/img/dice" + randomNumber2 + ".png")
+
+    if (randomNumber1 === randomNumber2) {
+
+        document.querySelector("h1").innerHTML = "DRAW" }
+    else if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML="PLAYER 1 WON" }
+    else {
+        document.querySelector("h1").innerHTML="PLAYER 2 WON"}
+
+} randomImages()
